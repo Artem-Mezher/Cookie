6844634b6c68810c4123d544c7dd4b8b1288bb25
Artem <ayuartem@gmail.com>
2022-05-07 00:43:50 +0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Cookie cookie = new Cookie();
+        cookie.bake();
+    }
+public static class Cookie {
+    public  void bake () {
+        System.out.println("Pechenka");
+
+
+    }
+}}
